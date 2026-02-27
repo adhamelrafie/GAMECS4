38554b9059abfa27f3f80fd358fa91b204d559a9
adhamelrafie <elrafieadham@gmail.com>
2026-02-27 10:16:39 +0200

    Add files via upload
    wala

---
+++ Mytestssss.java
@@ -0,0 +1,10 @@
+package game.tests;
+
+public class Mytestssss implements Comparable {
+	public int compareTo(Object x){
+		return 1;
+	}
+	public static void main(String[]args){
+		System.out.println(1);
+	}
+}
